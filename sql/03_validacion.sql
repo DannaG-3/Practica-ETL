f1e691501aff452992f2b7bcd2f1d75ce1d82e79
DannaG-3 <danna.glez.gx2@gmail.com>
2026-06-05 12:46:51 -0600

    Insert desde transformation hacia analytics

--- sql/03_validacion.sql
+++ sql/03_validacion.sql
@@ -41,3 +41,75 @@ CREATE TABLE analytics.orders (
     CONSTRAINT chk_order_year_four_digits
         CHECK (order_year BETWEEN 1000 AND 9999) -- El año del pedido debe ser un número de cuatro dígitos
 );
+
+
+-- Cargar datos en la tabla analytics.orders
+INSERT INTO analytics.orders (
+    record_id,
+    order_id,
+    order_date,
+    ship_date,
+    ship_mode,
+    customer_name,
+    segment,
+    state_name,
+    country,
+    market,
+    region,
+    product_id,
+    category,
+    sub_category,
+    product_name,
+    sales,
+    quantity,
+    discount,
+    profit,
+    shipping_cost,
+    order_priority,
+    order_year
+)
+SELECT
+    record_id,
+    order_id,
+    order_date,
+    ship_date,
+    ship_mode,
+    customer_name,
+    segment,
+    state_name,
+    country,
+    market,
+    region,
+    product_id,
+    category,
+    sub_category,
+    product_name,
+    sales,
+    quantity,
+    discount::NUMERIC(4,3),
+    profit::NUMERIC(12,5),
+    shipping_cost::NUMERIC(8,2),
+    order_priority,
+    order_year
+FROM transformation.orders
+WHERE order_id IS NOT NULL
+    AND order_date IS NOT NULL
+    AND ship_date IS NOT NULL
+    AND ship_mode IS NOT NULL
+    AND customer_name IS NOT NULL
+    AND segment IS NOT NULL
+    AND state_name IS NOT NULL
+    AND country IS NOT NULL
+    AND market IS NOT NULL
+    AND region IS NOT NULL
+    AND product_id IS NOT NULL
+    AND category IS NOT NULL
+    AND sub_category IS NOT NULL
+    AND product_name IS NOT NULL
+    AND sales IS NOT NULL
+    AND quantity IS NOT NULL
+    AND discount IS NOT NULL
+    AND profit IS NOT NULL
+    AND shipping_cost IS NOT NULL
+    AND order_priority IS NOT NULL
+    AND order_year IS NOT NULL;
