befbf8d18d31ca337fada5a4c8ae3099950a4717
Brayan Paul Pelayo Ascencio <brayan.pelayo@outlook.com>
2026-06-05 21:22:03 -0600

    .

--- sql/01_extraccion.sql
+++ sql/01_extraccion.sql
@@ -37,16 +37,9 @@ CREATE TABLE staging.orders (
     order_year TEXT
 );
 
-<<<<<<< HEAD
 -- Importar datos usando el cliente psql (\copy)
 -- NOTA: Debe ir en una sola línea, con diagonales normales (/) y SIN punto y coma al final.
 \copy staging.orders FROM 'datos/SuperStoreOrders.csv' WITH (FORMAT csv, HEADER true, ENCODING 'UTF8')
-=======
--- Importar datos desde el archivo CSV
-\copy staging.orders 
-FROM 'datos/SuperStoreOrders.csv' 
-WITH (FORMAT csv, HEADER true, ENCODING 'UTF8');
->>>>>>> d8ded083bc8014973c5783f920673214d5d4a779
 
 -- Verificar que los datos se hayan importado correctamente
 SELECT COUNT(*) FROM staging.orders; -- Esperamos 51290 filas
